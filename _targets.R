c4d5a896dcca5f40f1923b8ca5cf1de9e8955724
Anthony Martinez <ajmartinez@usgs.gov>
2022-06-05 15:50:09 -0700

    Add _targets.R and update gitignore

--- _targets.R
+++ _targets.R
@@ -1,35 +1,40 @@
 library(targets)
-source("code.R")
+source("1_fetch/src/fetch.R")
+source("2_process/src/process.R")
+source("3_visualize/src/visualize.R")
+source("3_visualize/src/write_diagnostics.R")
+
 tar_option_set(packages = c("tidyverse", "sbtools", "whisker"))
 
 list(
   # Get the data from ScienceBase
   tar_target(
     model_RMSEs_csv,
-    download_data(out_filepath = "model_RMSEs.csv"),
+    fetch(filepath_out = '1_fetch/out/model_RMSEs.csv'),
     format = "file"
   ), 
   # Prepare the data for plotting
   tar_target(
     eval_data,
-    process_data(in_filepath = model_RMSEs_csv),
+    process(filepath_in = model_RMSEs_csv,
+            filepath_out = '2_process/out/model_summary_results.csv',
+            viz_col = c('#1b9e77', '#d95f02', '#7570b3'),
+            viz_pch = c(21, 22, 23)),
+    format = "file"
   ),
   # Create a plot
   tar_target(
     figure_1_png,
-    make_plot(out_filepath = "figure_1.png", data = eval_data), 
+    visualize(filepath_in = eval_data,
+              filepath_out = '3_visualize/out/figure_1.png'), 
     format = "file"
   ),
-  # Save the processed data
-  tar_target(
-    model_summary_results_csv,
-    write_csv(eval_data, file = "model_summary_results.csv"), 
-    format = "file"
-  ),
+
   # Save the model diagnostics
   tar_target(
     model_diagnostic_text_txt,
-    generate_model_diagnostics(out_filepath = "model_diagnostic_text.txt", data = eval_data), 
+    write_diagnostics(filepath_in = eval_data,
+                      filepath_out = '3_visualize/out/model_diagnostic_text.txt'), 
     format = "file"
   )
 )
